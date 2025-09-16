77971f59a8d24d1e00fb1779b4c6e282b0db9d95
teddy <AAteddy@users.noreplay.github.com>
2025-09-16 17:56:30 +0300

    Feature: integrate secure auth middleware for accessing product-service endpoints

--- cmd/product-service/main.go
+++ cmd/product-service/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+
 	"github.com/AAteddy/go-ecommerce-app/internal/pkg/config"
 	"github.com/AAteddy/go-ecommerce-app/internal/pkg/db"
 	"github.com/AAteddy/go-ecommerce-app/internal/pkg/logging"
 	productHttp "github.com/AAteddy/go-ecommerce-app/internal/product/delivery/http"
 	"github.com/AAteddy/go-ecommerce-app/internal/product/repository"
 	"github.com/AAteddy/go-ecommerce-app/internal/product/usecase"
-	"github.com/go-chi/chi/v5"
+	"github.com/AAteddy/go-ecommerce-app/internal/user/infrastructure/redis"
+	userRepo "github.com/AAteddy/go-ecommerce-app/internal/user/repository"
+	UserUC "github.com/AAteddy/go-ecommerce-app/internal/user/usecase"
 )
 
 func main() {
@@ -23,11 +27,15 @@ func main() {
 		log.Fatal("Failed to connect to database", "error", err)
 	}
 
+	tokenStore := redis.NewRedisTokenStore(cfg.RedisAddr)
+	userRepo := userRepo.NewPostgresUserRepository(dbConn)
+	userUseCase := UserUC.NewUserUseCase(userRepo, tokenStore, nil, cfg.JWTSecret, log)
+
 	repo := repository.NewPostgresProductRepository(dbConn)
 	usecase := usecase.NewProductUseCase(repo, log)
 
 	route := chi.NewRouter()
-	handler := productHttp.NewProductHandler(usecase, log)
+	handler := productHttp.NewProductHandler(usecase, userUseCase, log)
 	handler.RegisterRoutes(route)
 
 	log.Info("Starting product service on port: " + cfg.ProductHTTPPort)
